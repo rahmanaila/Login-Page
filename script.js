2fc2965c00a47c52da241b7e1966c7c8a093ea0a
nai <142805070+rahmanaila@users.noreply.github.com>
2024-10-04 20:08:28 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Ambil nilai input
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // Validasi sederhana username dan password
+    if (username === 'admin' && password === '123456') {
+        alert('Login berhasil!');
+        // Refresh halaman setelah menekan "OK"
+        window.location.reload();
+    } else {
+        // Tampilkan pesan error jika login gagal
+        document.getElementById('errorMessage').textContent = 'Username atau password salah!';
+    }
+});
